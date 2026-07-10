016529d12d535dcdd373b1f992fcd5312038a3f3
amanfatima-hash <ua5841747@gmail.com>
2026-07-10 03:14:39 -0400

    Completed step 5

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -36,3 +36,7 @@ switch (userType) {
         userCategory = "Unknown";
 }
 console.log("User Category:", userCategory);
+// ternary operator 
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
